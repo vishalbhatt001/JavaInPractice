c10f5fbd1a539bc9d917fa0a3d44b97d770e2a16
vishalbhatt001 <vishalbhatt001@gmail.com>
2020-02-18 00:08:08 +0530

    Some tweak changes

--- src/main/java/com/practice/functioninterface/Main.java
+++ src/main/java/com/practice/functioninterface/Main.java
@@ -13,13 +13,18 @@ public class Main {
 
 		List<Model> modelList = new ArrayList<>(Arrays.asList(new Model("Nokia101"), null, new Model("Nokia102"), null, new Model("Nokia103")) );
 		List<Model> modelList2 = new ArrayList<>(Arrays.asList(new Model("Nokia201"), null, new Model("Nokia202"), null, new Model("Nokia203")) );
-		List<Brand> brandList = new ArrayList<>(Arrays.asList(new Brand(modelList), null, new Brand(modelList2)));
-		List<ItemType> itemtypes = new ArrayList<>(Arrays.asList(new ItemType(brandList), null));
+		Optional<List<Model>> op1 = Optional.ofNullable(modelList);
+		Optional<List<Model>> op2 = Optional.ofNullable(modelList2);
+		List<Brand> brandList = new ArrayList<>(Arrays.asList(new Brand(op1.orElse(op1.get())), null, new Brand(op2.orElse(op2.get()))));
+		Optional<List<Brand>> op3 = Optional.ofNullable(brandList);
+		List<ItemType> itemtypes = new ArrayList<>(Arrays.asList(new ItemType(op3.orElse(op3.get())), null));
 		
 		//itemtypes.forEach(e -> System.out.print(e.getBrands().get(0).getModels().get(0).getModel()));
 		
 		Optional<List<ItemType>> optionalsItems = Optional.ofNullable(itemtypes);
-		optionalsItems.get().stream().flatMap(e -> Optional.ofNullable(e.getBrands().stream()).get()).flatMap(e -> Optional.ofNullable(e.getModels().stream()).get()).forEach(e -> System.out.print(Optional.ofNullable(e.getModel()).get()));
+		optionalsItems.orElse(optionalsItems.get()).stream().flatMap(e -> e.getBrands().stream()).forEach(e -> System.out.print(e));
+		
+		//optionalsItems.orElse(optionalsItems.get()).stream().flatMap(e -> e.getBrands().stream()).flatMap(e -> e.getModels().stream()).forEach(e -> System.out.print(e.getModel()));
 	}
 
 }
